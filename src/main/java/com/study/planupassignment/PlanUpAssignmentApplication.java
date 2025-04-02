b4027b6df55365ac010ecc666bee69c91e257284
jhyang22 <jaeho9229@gmail.com>
2025-04-02 19:41:01 +0900

    과제 생성

---
+++ src/main/java/com/study/planupassignment/PlanUpAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.study.planupassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanUpAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanUpAssignmentApplication.class, args);
+    }
+
+}
